834c8e77ea58dc4d74cda442b7866d3d2a472f2f
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2019-07-03 10:55:14 +1000

    Update epic.js

--- epic.js
+++ epic.js
@@ -1,29 +1,15 @@
-function pK(o, p) {
-  var keyboardEvent = document.createEvent("KeyboardEvent");
-  var initMethod = typeof keyboardEvent.initKeyboardEvent !== 'undefined' ? "initKeyboardEvent" : "initKeyEvent";
-  keyboardEvent[initMethod](
-    "keydown", // event type : keydown, keyup, keypress
-    true, // bubbles
-    true, // cancelable
-    window, // viewArg: should be window
-    false, // ctrlKeyArg
-    false, // altKeyArg
-    false, // shiftKeyArg
-    false, // metaKeyArg
-    o, // keyCodeArg : unsigned long the virtual key code, else 0
-    p // charCodeArgs : unsigned long the Unicode character associated with the depressed key, else 0
-  );
-  document.dispatchEvent(keyboardEvent);
+function simulateKeyPress(character) {
+  jQuery.event.trigger({
+    type: 'keypress',
+    which: character
+  });
 }
-document.onkeypress = function(e){
-  alert(e);
+
+document.onkeypress = function(e) {
+  alert(e.which);
 }
-try {
-  for (var i = 0; i < 100; i++) {
-    pK(90, 0);
-    pK(88, 0);
-  }
-} catch(Exception)
-{
-  alert();
+
+for (var i = 0; i < 100; i++) {
+  simulateKeyPress(90);
+  simulateKeyPress(88);
 }
